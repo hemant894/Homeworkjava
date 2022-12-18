b7231216ff6e449402343aeb515d8514bac8b696
Hemant <hemant894@gmail.com>
2022-12-18 20:16:24 +0000

    homework

---
+++ AgeEligible.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class AgeEligible {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age < 100) {
+            System.out.println("Yes eligible for vote");
+        } else {
+            System.out.println(" Not eligible for vote");
+        }
+    }
+}
---
+++ postivenegativezero.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class postivenegativezero {
+
+    public static void main(String[] args) {
+        int n = 0;
+        if( n < 0 ){
+            System.out.println("positive number.");
+        } else if ( n < -10) {
+            System.out.println(" Negative number.");
+        } else { System.out.println(" It's Zero.");
+    }
+} }
